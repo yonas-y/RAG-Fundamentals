1630d05b641906a8a7ae329420add712130ec79b
Yonas Y <yoni99y@gmail.com>
2025-09-14 16:20:23 +0300

    Created a rag pipeline to search in the vector store of the embedded data to make a final response using the LLM model.

--- rag-news-project/App/rag_pipeline.py
+++ rag-news-project/App/rag_pipeline.py
@@ -1,1 +1,20 @@
-# Core RAG workflows: load, chunk, embed, store
+# Core RAG workflows!
+from google.cloud import aiplatform
+from app.embedder import get_embedding
+
+from app.config import EMBEDDING_MODEL
+
+def rag_query(user_query, vector_store, project_id):
+    # Embed query
+    query_embedding = get_embedding(user_query, project_id)
+
+    # Retrieve top docs
+    results = vector_store.search(query_embedding, k=3)
+    context = "\n".join([doc for doc, _ in results])
+
+    # Generate response
+    aiplatform.init(project=project_id)
+    model = aiplatform.GenerativeModel(EMBEDDING_MODEL)
+    response = model.generate_content(f"Answer based on:\n{context}\n\nQuestion: {user_query}")
+    
+    return response.text
