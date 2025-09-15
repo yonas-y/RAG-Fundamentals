90305302feb1369a875d10cf32a121414f7d153e
Yonas Y <yoni99y@gmail.com>
2025-09-15 17:00:04 +0300

    Added a directory path for persistance storage.

--- rag-news-project/App/config.py
+++ rag-news-project/App/config.py
@@ -3,4 +3,5 @@ PROJECT_ID = "rag-fundamentals-471014"
 LOCATION = "us-central1"
 EMBEDDING_MODEL = "text-embedding-004"
 
-doc_dir_path = "data/"
+doc_dir_path = "data/news_articles/"
+PERSIST_DIR = "vector_store/"
